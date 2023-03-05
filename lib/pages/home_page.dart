8a1663e36e16b324e307d859a571a64630dd4cec
Hajar Aswad <hajaraswadkom@gmail.com>
2023-03-05 12:03:53 +0700

    feat: tips and guides

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:cozy_kost/models/space.dart';
+import 'package:cozy_kost/models/tips.dart';
 import 'package:cozy_kost/theme.dart';
 import 'package:cozy_kost/widgets/city_card.dart';
 import 'package:cozy_kost/widgets/space_card.dart';
+import 'package:cozy_kost/widgets/tips_card.dart';
 import 'package:flutter/material.dart';
 import 'package:cozy_kost/models/city.dart';
 import 'package:cozy_kost/models/space.dart';
@@ -12,142 +14,178 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         bottom: false,
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: edge),
-          child: ListView(
-            children: [
-              // NOTE: TITLE/HEADER
-              Padding(
-                padding: EdgeInsets.only(left: edge),
-                child: Text(
-                  'Explore Now',
-                  style: blackTextStyle.copyWith(fontSize: 24),
-                ),
+        child: ListView(
+          children: [
+            SizedBox(
+              height: edge,
+            ),
+            // NOTE: TITLE/HEADER
+            Padding(
+              padding: EdgeInsets.only(left: edge),
+              child: Text(
+                'Explore Now',
+                style: blackTextStyle.copyWith(fontSize: 24),
               ),
-              SizedBox(height: 2),
-              Padding(
-                padding: EdgeInsets.only(
-                  left: edge,
-                ),
-                child: Text(
-                  'Mencari kosan yang cozy',
-                  style: greyTextStyle.copyWith(fontSize: 16),
-                ),
+            ),
+            SizedBox(height: 2),
+            Padding(
+              padding: EdgeInsets.only(
+                left: edge,
               ),
-              SizedBox(height: 30),
-              // NOTE: POPULAR CITIES
-              Padding(
-                padding: EdgeInsets.only(
-                  left: edge,
-                ),
-                child: Text(
-                  'Popular Cities',
-                  style: redularTextStyle.copyWith(fontSize: 16),
-                ),
+              child: Text(
+                'Mencari kosan yang cozy',
+                style: greyTextStyle.copyWith(fontSize: 16),
               ),
-              SizedBox(height: 16),
-              Container(
-                height: 150,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    const SizedBox(width: 24),
-                    CityCard(
-                      City(
-                        id: 1,
-                        name: 'Jakarta',
-                        image_url: 'assets/images/city1.png',
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    CityCard(
-                      City(
-                        id: 1,
-                        name: 'Bandung',
-                        image_url: 'assets/images/city2.png',
-                        isPopular: true,
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    CityCard(
-                      City(
-                        id: 1,
-                        name: 'Surabaya',
-                        image_url: 'assets/images/city3.png',
-                      ),
-                    ),
-                    const SizedBox(width: 24),
-                  ],
-                ),
+            ),
+            SizedBox(height: 30),
+            // NOTE: POPULAR CITIES
+            Padding(
+              padding: EdgeInsets.only(
+                left: edge,
               ),
-              SizedBox(height: 30),
-              // NOTE: RECOMMENDED SPACE
-              Padding(
-                padding: EdgeInsets.only(
-                  left: edge,
-                ),
-                child: Text(
-                  'Recommended Space',
-                  style: redularTextStyle.copyWith(fontSize: 16),
-                ),
+              child: Text(
+                'Popular Cities',
+                style: redularTextStyle.copyWith(fontSize: 16),
               ),
-              SizedBox(height: 16),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: edge),
-                child: Column(
-                  children: [
-                    SpaceCard(
-                      Space(
-                        id: 1,
-                        name: 'Kuretakeso Hott',
-                        imageUrl: 'assets/images/space1.png',
-                        price: 52,
-                        city: 'Bandung',
-                        country: 'Indonesia',
-                        rating: 4,
-                      ),
+            ),
+            SizedBox(height: 16),
+            Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  const SizedBox(width: 24),
+                  CityCard(
+                    City(
+                      id: 1,
+                      name: 'Jakarta',
+                      image_url: 'assets/images/city1.png',
                     ),
-                    SizedBox(height: 30),
-                    SpaceCard(
-                      Space(
-                        id: 1,
-                        name: 'Kuretakeso Hott',
-                        imageUrl: 'assets/images/space1.png',
-                        price: 52,
-                        city: 'Bandung',
-                        country: 'Indonesia',
-                        rating: 4,
-                      ),
+                  ),
+                  const SizedBox(width: 20),
+                  CityCard(
+                    City(
+                      id: 1,
+                      name: 'Bandung',
+                      image_url: 'assets/images/city2.png',
+                      isPopular: true,
                     ),
-                    SizedBox(height: 30),
-                    SpaceCard(
-                      Space(
-                        id: 1,
-                        name: 'Roemah Nenek',
-                        imageUrl: 'assets/images/space1.png',
-                        price: 11,
-                        city: 'Seattle',
-                        country: 'Bogor',
-                        rating: 4,
-                      ),
+                  ),
+                  const SizedBox(width: 20),
+                  CityCard(
+                    City(
+                      id: 1,
+                      name: 'Surabaya',
+                      image_url: 'assets/images/city3.png',
                     ),
-                    SizedBox(height: 30),
-                    SpaceCard(
-                      Space(
-                        id: 1,
-                        name: 'Kuretakeso Hott',
-                        imageUrl: 'assets/images/space1.png',
-                        price: 20,
-                        city: 'Jakarta',
-                        country: 'Indonesia',
-                        rating: 4,
-                      ),
+                  ),
+                  const SizedBox(width: 24),
+                ],
+              ),
+            ),
+            SizedBox(height: 30),
+            // NOTE: RECOMMENDED SPACE
+            Padding(
+              padding: EdgeInsets.only(
+                left: edge,
+              ),
+              child: Text(
+                'Recommended Space',
+                style: redularTextStyle.copyWith(fontSize: 16),
+              ),
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: edge),
+              child: Column(
+                children: [
+                  SpaceCard(
+                    Space(
+                      id: 1,
+                      name: 'Kuretakeso Hott',
+                      imageUrl: 'assets/images/space1.png',
+                      price: 52,
+                      city: 'Bandung',
+                      country: 'Indonesia',
+                      rating: 4,
                     ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  ),
+                  SizedBox(height: 30),
+                  SpaceCard(
+                    Space(
+                      id: 1,
+                      name: 'Kuretakeso Hott',
+                      imageUrl: 'assets/images/space1.png',
+                      price: 52,
+                      city: 'Bandung',
+                      country: 'Indonesia',
+                      rating: 4,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  SpaceCard(
+                    Space(
+                      id: 1,
+                      name: 'Roemah Nenek',
+                      imageUrl: 'assets/images/space1.png',
+                      price: 11,
+                      city: 'Seattle',
+                      country: 'Bogor',
+                      rating: 4,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  SpaceCard(
+                    Space(
+                      id: 1,
+                      name: 'Kuretakeso Hott',
+                      imageUrl: 'assets/images/space1.png',
+                      price: 20,
+                      city: 'Jakarta',
+                      country: 'Indonesia',
+                      rating: 4,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 30),
+            // NOTE: TIPS & GUIDANCE
+            Padding(
+              padding: EdgeInsets.only(
+                left: edge,
+              ),
+              child: Text(
+                'Tips & Guidance',
+                style: redularTextStyle.copyWith(fontSize: 16),
+              ),
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: edge),
+              child: Column(
+                children: [
+                  TipsCard(
+                    Tips(
+                      id: 1,
+                      title: 'City Guidelines',
+                      imageUrl: 'assets/images/tips1.png',
+                      updatedAt: '20 Apr',
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TipsCard(
+                    Tips(
+                      id: 1,
+                      title: 'Jakarta Fairship',
+                      imageUrl: 'assets/images/tips2.png',
+                      updatedAt: '11 Dec',
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
---
+++ lib/widgets/tips_card.dart
@@ -0,0 +1,44 @@
+import 'package:cozy_kost/models/tips.dart';
+import 'package:cozy_kost/theme.dart';
+import 'package:flutter/material.dart';
+
+class TipsCard extends StatelessWidget {
+  Tips tips;
+
+  TipsCard(this.tips);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Image.asset(
+          tips.imageUrl,
+          width: 80,
+        ),
+        SizedBox(width: 16),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              tips.title,
+              style: blackTextStyle.copyWith(fontSize: 18),
+            ),
+            SizedBox(height: 4),
+            Text(
+              'Updated ${tips.updatedAt}',
+              style: greyTextStyle,
+            ),
+          ],
+        ),
+        Spacer(),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.chevron_right,
+            color: greyColor,
+          ),
+        )
+      ],
+    );
+  }
+}
